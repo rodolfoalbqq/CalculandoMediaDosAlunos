ad704363e8ed477e0c1297f7d47640cf2677abb6
Rodolfo Albuquerque <rodolfo.albqq@gmail.com>
2022-08-16 09:23:30 -0300

    v.1.0

---
+++ NewConsole/Program.cs
@@ -0,0 +1,19 @@
+﻿double a = 0;
+double b = 0;
+double c = 0;
+double d = 0;
+double e = 0;
+
+Console.WriteLine("Informe a 1° nota");
+a = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Informe a 2° nota");
+b = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Informe a 3° nota");
+c = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Informe a 4° nota");
+d = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Informe a 5° nota");
+e = Convert.ToDouble(Console.ReadLine());
+
+double media = (a + b + c + d + e) / 5;
+Console.WriteLine("A média de nota é " + media);
